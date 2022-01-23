7b698d3238a94a21a64f97bf8a83d9352b368cd3
Gokul NC <gokulnc@ymail.com>
2022-01-23 22:41:19 +0530

    Port Subin's code to see if it works
    And it does!

--- source/scripts/popup.js
+++ source/scripts/popup.js
@@ -1,38 +1,97 @@
-import 'emoji-log';
+/*
+ * Indic-En : WebExtension to transliterate webpages
+ * https://subinsb.com/indicen
+ *
+ * This work is licensed under GNU General Public License version 3.
+ * 
+ * Copyright 2020 Subin Siby <mail@subinsb.com>
+*/
+
 import browser from 'webextension-polyfill';
 
 import '../styles/popup.scss';
 
-function openWebPage(url) {
-  return browser.tabs.create({url});
-}
-
 document.addEventListener('DOMContentLoaded', async () => {
-  const tabs = await browser.tabs.query({
-    active: true,
-    lastFocusedWindow: true,
+  const lang_elem = document.getElementById('lang');
+  var lang = 'ml';
+
+  // Restore options
+  browser.storage.sync.get({
+    auto: false,
+    overlay: true,
+    lang: lang
+  }).then((result) => {
+    document.getElementById('auto_transliterate').checked = result.auto;
+    document.getElementById('overlay').checked = result.overlay;
+
+    if (result.lang.length === 2) {
+      lang = result.lang;
+      lang_elem.selectedIndex = document.querySelector(`[value=${lang}]`).index;
+    }
+  }, (err) => {
+    console.log(err);
   });
 
-  const url = tabs.length && tabs[0].url;
+  // There will be only one active tab
+  let tab = await browser.tabs.query({
+    active: true
+  });
+  tab = tab[0]
 
-  const response = await browser.runtime.sendMessage({
-    msg: 'hello',
-    url,
+  document.getElementById('transliterate').addEventListener('click', async () => {
+    browser.tabs.sendMessage(
+      tab.id,
+      {
+        lang: lang
+      }
+    ).catch(error => {
+      console.log(error);
+    });
+    document.getElementById('transliterate').style.display = 'none'
+    document.getElementById('untransliterate').style.display = 'inline'
   });
 
-  console.emoji('🦄', response);
-
-  document.getElementById('github__button').addEventListener('click', () => {
-    return openWebPage(
-      'https://github.com/abhijithvijayan/web-extension-starter'
+  document.getElementById('untransliterate').addEventListener('click', async () => {
+    browser.tabs.sendMessage(
+      tab.id,
+      {
+        untransliterate: true
+      }
     );
+    document.getElementById('transliterate').style.display = 'inline'
+    document.getElementById('untransliterate').style.display = 'none'
   });
 
-  document.getElementById('donate__button').addEventListener('click', () => {
-    return openWebPage('https://www.buymeacoffee.com/abhijithvijayan');
+  // Check if page transliterated (auto mode on)
+  browser.tabs.sendMessage(
+    tab.id,
+    {
+      transliterated_webpage: '?'
+    }
+  ).then(transliterated_webpage => {
+    if (transliterated_webpage) {
+      document.getElementById('transliterate').style.display = 'none'
+      document.getElementById('untransliterate').style.display = 'inline'
+    }
   });
 
-  document.getElementById('options__button').addEventListener('click', () => {
-    return openWebPage('options.html');
+  var save_settings = () => {
+    browser.storage.sync.set({
+      auto: document.getElementById('auto_transliterate').checked,
+      overlay: document.getElementById('overlay').checked,
+      lang: lang
+    });
+  }
+
+  lang_elem.addEventListener('change', () => {
+    lang = lang_elem.value;
+    save_settings();
+  });
+
+  document.getElementById('auto_transliterate').addEventListener('change', () => {
+    save_settings();
+  });
+  document.getElementById('overlay').addEventListener('change', () => {
+    save_settings();
   });
 });
